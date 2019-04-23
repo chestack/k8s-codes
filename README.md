e2b836a59ea32c6d4b521ad75ce4b6a14f77ba7f
chestack <wangbo_bupt@163.com>
2019-04-23 22:58:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# here are some sample codes about k8s
+
+# main/usecase-informer.go is a sample about informer
+# main/c-controller.go is a sample about custom controller
+
+# go build and run it on any k8s cluster
---
+++ listwatch/main/usecase-informer.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/informers"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/cache"
+    "k8s.io/client-go/tools/clientcmd"
+    "log"
+)
+
+func main() {
+    kubeconfig := "/root/kubeconfig"
+    config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    factory := informers.NewSharedInformerFactory(clientset, 0)
+    informer := factory.Core().V1().Pods().Informer()
+    stopper := make(chan struct{})
+    defer close(stopper)
+
+    informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+        AddFunc: func(obj interface{}) {
+            // "k8s.io/apimachinery/pkg/apis/meta/v1" provides an Object
+            // interface that allows us to get metadata easily
+            mObj := obj.(v1.Object)
+            log.Printf("New Pod Added to Store: %s", mObj.GetName())
+        },
+    })
+    informer.Run(stopper)
+}
